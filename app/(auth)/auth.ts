7b7f9404f39d74b8b8717e1a1c277834daecb273
Adil ABBADI <adil.abbadi.1996@gmail.com>
2025-03-25 02:45:04 +0000

    verifier disable

--- app/(auth)/auth.ts
+++ app/(auth)/auth.ts
@@ -33,6 +33,7 @@ export const {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      checks: ["none"],
     }),
   ],
   callbacks: {
